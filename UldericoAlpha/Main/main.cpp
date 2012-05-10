4a180b1f5984559033aa6ee2f4cfb9be32b1474c
Thomas.Buning@gmail.com <Thomas.Buning@gmail.com@bcffac1d-9c80-acbf-a401-686821d305f5>
2012-05-10 10:37:52 +0000

    Added ressources

--- UldericoAlpha/Main/main.cpp
+++ UldericoAlpha/Main/main.cpp
@@ -1,29 +1,41 @@
- #include <SFML/Audio.hpp>
- #include <SFML/Graphics.hpp>
- 
- int main()
- {
-     // Create the main window
-     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
- 
-     // Start the game loop
-	 while (window.IsOpen())
-     {
-         // Process events
-         sf::Event event;
-         while (window.PollEvent(event))
-         {
-             // Close window : exit
-			 if (event.Type == sf::Event::Closed)
-                 window.Close();
-         }
- 
-         // Clear screen
-         window.Clear();
- 
-         // Update the window
-         window.Display();
-     }
- 
-     return EXIT_SUCCESS;
- }
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+	// Create the main window
+	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+	
+	//Load background image
+	sf::Texture texture;
+	if(!texture.LoadFromFile(""))
+		return EXIT_FAILURE;
+
+	//Load background music
+	sf::Music bgMusic;
+	if(!bgMusic.OpenFromFile(""))
+		return EXIT_FAILURE;
+	//Play background music
+	bgMusic.Play();
+
+	// Start the game loop
+	while (window.IsOpen())
+	{
+		// Process events
+		sf::Event event;
+		while (window.PollEvent(event))
+		{
+			// Close window : exit
+			if (event.Type == sf::Event::Closed)
+				window.Close();
+		}
+
+		// Clear screen
+		window.Clear();
+
+		// Update the window
+		window.Display();
+	}
+
+	return EXIT_SUCCESS;
+}
