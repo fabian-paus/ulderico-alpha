d359a921f44ddec58941a1f092f2fc950a042321
paus.fabian@gmail.com <paus.fabian@gmail.com@bcffac1d-9c80-acbf-a401-686821d305f5>
2012-05-11 08:58:02 +0000

    Load images

--- trunk/UldericoAlpha/Main/ResourcesManager.cpp
+++ trunk/UldericoAlpha/Main/ResourcesManager.cpp
@@ -1,22 +1,20 @@
 #include "ResourcesManager.h"
 
-static const char *characters = "\\res\\img\\characters.png";
-static const char *background = "\\res\\img\\invaders.png";
+static const char* const CHARACTERS_IMAGE = "res/img/characters.png";
+static const char* const BACKGROUND_IMAGE = "res/img/invaders.png";
 
 ResourcesManager::ResourcesManager()
 {
-	//Load all images into sprites and make them accessable by name
-	sf::Texture bg;
-	sf::Texture character;
-	if(!bg.LoadFromFile(background) || !character.LoadFromFile(characters))
+	// Load all images into sprites and make them accessable by name
+	if(!m_background.LoadFromFile(BACKGROUND_IMAGE) || !m_character.LoadFromFile(CHARACTERS_IMAGE))
 		return;
-	sf::Sprite back(bg);
-	RegisterSprite("background", back);
+
+	RegisterSprite("background", sf::Sprite(m_background));
 }
 
 void ResourcesManager::RegisterSprite(std::string const & name, sf::Sprite resource)
 {
-	spriteResources.insert(std::make_pair(name, resource));
+	m_spriteResources.insert(std::make_pair(name, resource));
 }
 
 //void ResourcesManager::RegisterSound(std::string const & name, sf::Sound resource)
@@ -26,7 +24,7 @@ void ResourcesManager::RegisterSprite(std::string const & name, sf::Sprite resou
 
 const sf::Sprite& ResourcesManager::GetSprite(std::string const & name)
 {
-	return spriteResources[name];
+	return m_spriteResources[name];
 }
 
 //const sf::Sound& ResourcesManager::GetSound(std::string const & name)
