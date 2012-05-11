1d8bf6f38cfe07d1077af11ee12b8550b4c6bb3c
Thomas.Buning@gmail.com <Thomas.Buning@gmail.com@bcffac1d-9c80-acbf-a401-686821d305f5>
2012-05-11 07:46:07 +0000

    deleted sound

--- trunk/UldericoAlpha/Main/ResourcesManager.cpp
+++ trunk/UldericoAlpha/Main/ResourcesManager.cpp
@@ -3,7 +3,7 @@
 static const char *characters = "\\res\\img\\characters.png";
 static const char *background = "\\res\\img\\invaders.png";
 
-ResourcesManager::ResourcesManager(void)
+ResourcesManager::ResourcesManager()
 {
 	//Load all images into sprites and make them accessable by name
 	sf::Texture bg;
@@ -14,22 +14,22 @@ ResourcesManager::ResourcesManager(void)
 	RegisterSprite("background", back);
 }
 
-void ResourcesManager::RegisterSprite( std::string const & name, sf::Sprite resource )
+void ResourcesManager::RegisterSprite(std::string const & name, sf::Sprite resource)
 {
 	spriteResources.insert(std::make_pair(name, resource));
 }
 
-void ResourcesManager::RegisterSound( std::string const & name, sf::Sound resource )
-{
-	soundResources.insert(std::make_pair(name, resource));
-}
+//void ResourcesManager::RegisterSound(std::string const & name, sf::Sound resource)
+//{
+//	soundResources.insert(std::make_pair(name, resource));
+//}
 
 const sf::Sprite& ResourcesManager::GetSprite(std::string const & name)
 {
 	return spriteResources[name];
 }
 
-const sf::Sound& ResourcesManager::GetSound(std::string const & name)
-{
-	return soundResources[name];
-}
+//const sf::Sound& ResourcesManager::GetSound(std::string const & name)
+//{
+//	return soundResources[name];
+//}
--- trunk/UldericoAlpha/Main/ResourcesManager.h
+++ trunk/UldericoAlpha/Main/ResourcesManager.h
@@ -12,17 +12,17 @@
 class ResourcesManager
 {
 	typedef std::map <std::string, sf::Sprite> SpriteResourceMap;
-	typedef std::map <std::string, sf::Sound> SoundResourceMap;
+	//typedef std::map <std::string, sf::Sound> SoundResourceMap;
 
 private:
 	SpriteResourceMap spriteResources;
-	SoundResourceMap soundResources;
+	//SoundResourceMap soundResources;
 	void RegisterSprite(std::string const & name, sf::Sprite resource );
-	void RegisterSound(std::string const & name, sf::Sound resource );
+	//void RegisterSound(std::string const & name, sf::Sound resource );
 public:
 	ResourcesManager();
 	const sf::Sprite& GetSprite(std::string const & name);
-	const sf::Sound& GetSound(std::string const & name);
+	//const sf::Sound& GetSound(std::string const & name);
 };
 
 #endif
