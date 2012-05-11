44e4427559cef189243d7cff6b83707da886e8c1
paus.fabian@gmail.com <paus.fabian@gmail.com@bcffac1d-9c80-acbf-a401-686821d305f5>
2012-05-11 14:45:22 +0000

    Loading sprites refactored

--- trunk/UldericoAlpha/Main/ResourcesManager.h
+++ trunk/UldericoAlpha/Main/ResourcesManager.h
@@ -14,27 +14,33 @@ class ResourcesManager
 public:
 	ResourcesManager();
 
-	const sf::Sprite& GetSprite(std::string const & name);
+	const sf::Sprite& GetSprite(std::string const& name);
 
-	const sf::Sound& GetSound(std::string const & name);
+	const sf::Sound& GetSound(std::string const& name);
 
 private:
-	void RegisterSprite(std::string const & name, sf::Sprite resource);
-
-	void RegisterSound(std::string const & name, sf::Sound resource);
-
-	void LoadImages();
+	void LoadSprites();
 
 	void LoadSounds();
 
+	void LoadSprite(std::string const& name, 
+		sf::Texture const& texture,
+		sf::IntRect const& textureRect);
+
 	void LoadSound(std::string const& name, std::string const& path);
+
+	void RegisterSprite(std::string const& name, sf::Sprite const& resource);
+
+	void RegisterSound(std::string const& name, sf::Sound const& resource);
+
+	void LoadTexture(sf::Texture& texture, std::string const& path);
 
 private:
 	typedef std::map <std::string, sf::Sprite> SpriteResourceMap;
 	typedef std::map <std::string, sf::Sound> SoundResourceMap;
 
 	sf::Texture m_background;
-	sf::Texture m_character;
+	sf::Texture m_characters;
 
 	std::list<sf::SoundBuffer> m_soundBuffers;
 
